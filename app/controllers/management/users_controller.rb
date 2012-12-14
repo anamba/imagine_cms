dd60b7fe5d730117d0d5315f480a86b65b622cd6
Aaron Namba <aaron@biggerbird.com>
2012-12-15 07:34:59 +0900

    sort users (active desc, username)

--- app/controllers/management/users_controller.rb
+++ app/controllers/management/users_controller.rb
@@ -10,7 +10,7 @@ class Management::UsersController < Management::ApplicationController
   ###
   
   def index
-    @users = User.all
+    @users = User.order('active desc, username').all
   end
   
   def new
