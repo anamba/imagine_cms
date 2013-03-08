3b303fd6e616f0984c54a52b71c9bc1449aaba3f
Aaron Namba <aaron@biggerbird.com>
2013-03-08 12:49:36 +0900

    allow mass assignment of tag name

--- app/models/cms_page_tag.rb
+++ app/models/cms_page_tag.rb
@@ -1,4 +1,6 @@
 class CmsPageTag < ActiveRecord::Base
+  attr_accessible :name
+  
   belongs_to :page, :class_name => 'CmsPage', :foreign_key => 'cms_page_id'
   
   validates_uniqueness_of :name, :scope => 'cms_page_id'
