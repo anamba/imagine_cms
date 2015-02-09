afb01539b896ac051aa4c9d4572b6297815df50a
Aaron Namba <aaron@biggerbird.com>
2015-02-09 09:26:04 +0900

    added missing includes to imagine_cms_compat.js

--- app/assets/javascripts/imagine_cms_compat.js
+++ app/assets/javascripts/imagine_cms_compat.js
@@ -11,9 +11,13 @@
 //= require codemirror/modes/xml
 //= require codemirror/modes/javascript
 //= require codemirror/modes/css
+//= require codemirror/modes/ruby
 //= require codemirror/modes/htmlmixed
+// require codemirror/modes/htmlembedded
+//= require codemirror/modes-custom/imagine_cms
 //
 //= require imagine_cms/core
 //= require imagine_cms/legacy/rollovers
 //= require imagine_cms/legacy/slideshow
 //= require imagine_cms/legacy/textfieldhints
+//= require imagine_cms/legacy/misc
