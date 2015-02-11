efddd559a8315549e912d6aa7977c55eab2e660a
Aaron Namba <aaron@biggerbird.com>
2015-02-11 10:53:13 +0900

    ignore exceptions that get generated when trying to clear cache from something other than a controller

--- app/sweepers/cms_content_sweeper.rb
+++ app/sweepers/cms_content_sweeper.rb
@@ -10,16 +10,20 @@ class CmsContentSweeper < ActionController::Caching::Sweeper
   end
   
   def delete_all_cached_pages
-    if File.expand_path(Management::CmsController.page_cache_directory) == File.expand_path("#{Rails.root}/public")
-      # expire home page
-      expire_page controller: 'cms/content', action: 'show', content_path: nil
+    begin
+      if File.expand_path(Management::CmsController.page_cache_directory) == File.expand_path("#{Rails.root}/public")
+        # expire home page
+        expire_page controller: 'cms/content', action: 'show', content_path: nil
       
-      # expire all other pages
-      CmsPage.select([ :id, :path ]).find_each do |page|
-        expire_page controller: 'cms/content', action: 'show', content_path: page.path.split('/')
+        # expire all other pages
+        CmsPage.select([ :id, :path ]).find_each do |page|
+          expire_page controller: 'cms/content', action: 'show', content_path: page.path.split('/')
+        end
+      else
+        FileUtils.rm_r(Dir.glob("#{cache_dir}/*")) rescue Errno::ENOENT
       end
-    else
-      FileUtils.rm_r(Dir.glob("#{cache_dir}/*")) rescue Errno::ENOENT
+    rescue Exception => e
+      Rails.logger.error "Error while clearing cache: #{e.message}" unless e.is_a?(NoMethodError)
     end
   end
   
