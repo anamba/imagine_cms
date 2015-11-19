6b5e9316f254ede9db0d345b75ef7c26357f2486
Aaron Namba <aaron@biggerbird.com>
2015-11-19 23:05:17 +0900

    more fixes for engine initialization process

--- lib/imagine_cms/engine.rb
+++ lib/imagine_cms/engine.rb
@@ -1,3 +1,35 @@
+require 'safe_yaml'
+
+require 'rails-observers'
+require 'actionpack/action_caching'
+require 'actionpack/page_caching'
+
+require 'prototype-rails'
+require 'prototype_legacy_helper/lib/prototype_legacy_helper'
+
+require 'non-stupid-digest-assets'
+
+require 'upload_progress/lib/multipart_progress'
+require 'upload_progress/lib/progress'
+require 'upload_progress/lib/upload_progress'
+require 'upload_progress/lib/upload_progress_helper'
+
+require 'acts_as_versioned/lib/acts_as_versioned'
+require 'aws-sdk'
+require 'zip'
+require 'rails_rinku'
+require 'acts_as_tree'
+require 'net/dns'
+require 'codemirror-rails'
+require 'RMagick'
+require 'mini_magick'
+
+require 'auto_link_email_addresses'
+require 'extensions/array_extensions'
+require 'hash_object'
+require 'hash_wrapper'
+
+
 module ImagineCms
   
   class Engine < Rails::Engine
@@ -6,10 +38,18 @@ module ImagineCms
     config.app_root = root
     middleware.use ::ActionDispatch::Static, "#{root}/public"
     
-    initializer "imagine_cms.assets.precompile" do |config|
-      Rails.application.config.assets.precompile += %w( dojo/** management.css imagine_controls.css reset.css )
-      Rails.application.config.assets.precompile += ["codemirror*", "codemirror/**/*"]
+    initializer "imagine_cms.assets.precompile" do |app|
+      app.config.assets.precompile += %w( dojo/** management.css imagine_controls.css reset.css )
+      app.config.assets.precompile += ["codemirror*", "codemirror/**/*"]
       # Rails.application.config.load_paths << File.dirname(__FILE__) + "/../app/helpers"
+    end
+    
+    initializer 'imagine_cms.legacy_support' do |app|
+      ActionController::Base.send :include, PrototypeHelper
+      ActionController::Base.send :helper, PrototypeHelper
+      
+      ActionController::Base.send(:include, UploadProgress)
+      ActionView::Base.send(:include, UploadProgress::UploadProgressHelper)
     end
     
     initializer 'imagine_cms.load_helpers' do |app|
@@ -32,69 +72,17 @@ module ImagineCms
       ActionController::Base.send :before_filter, :expire_session_data
     end
     
-    initializer 'imagine_cms.prototype_legacy_helper' do |app|
-      require 'prototype_legacy_helper/lib/prototype_legacy_helper'
-      ActionController::Base.send :include, PrototypeHelper
-      ActionController::Base.send :helper, PrototypeHelper
-    end
-    
-    initializer 'imagine_cms.acts_as_versioned' do |app|
-      require 'acts_as_versioned/lib/acts_as_versioned'
-    end
-    
-    initializer 'imagine_cms.upload_progress' do |app|
-      require 'upload_progress/lib/multipart_progress'
-      require 'upload_progress/lib/progress'
-      require 'upload_progress/lib/upload_progress'
-      require 'upload_progress/lib/upload_progress_helper'
-      ActionController::Base.send(:include, UploadProgress)
-      ActionView::Base.send(:include, UploadProgress::UploadProgressHelper)
-    end
-    
-    initializer 'imagine_cms.auto_link_email_addresses' do |app|
-      require 'auto_link_email_addresses'
-    end
-    
-    initializer 'imagine_cms.array_extensions' do |app|
-      require 'extensions/array_extensions'
-    end
-    
-    initializer 'imagine_cms.hash_classes' do |app|
-      require 'hash_object'
-      require 'hash_wrapper'
-    end
-    
     
     def self.activate
       Dir.glob(File.join(Rails.root, "app/overrides/*.rb")) do |c|
-        Rails.application.config.cache_classes ? require(c) : load(c)
+        require_dependency(c)
       end
       Dir.glob(File.join(Rails.root, "app/**/*_decorator*.rb")) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
+        require_dependency(c)
       end
     end
     
     config.to_prepare &method(:activate).to_proc
-    
-    #
-    # activate gems as needed
-    #
-    require 'rails-observers'
-    require 'actionpack/action_caching'
-    require 'actionpack/page_caching'
-    require 'aws-sdk'
-    require 'zip'
-    require 'rails_rinku'
-    require 'safe_yaml'
-    require 'acts_as_tree'
-    require 'net/dns'
-    require 'codemirror-rails'
-    
-    # on the way out, but still needed for now
-    require 'prototype-rails'
-    require 'RMagick'
-    require 'mini_magick'
-    require 'non-stupid-digest-assets'
   end
   
 end
