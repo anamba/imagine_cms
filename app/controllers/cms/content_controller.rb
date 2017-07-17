ade9d944516112dc7920712670913edd75917c79
Aaron Namba <aaron@aaronnamba.com>
2017-07-17 07:54:56 +0000

    adjusted underscore to hyphen translation regex

--- app/controllers/cms/content_controller.rb
+++ app/controllers/cms/content_controller.rb
@@ -149,7 +149,7 @@ module Cms # :nodoc:
           params[:page] = 'index'
         end
         
-        if @pg = (CmsPage.includes(:template).find_by_path(db_path.join('/')) || CmsPage.includes(:template).find_by_path(db_path.map { |segment| segment.gsub('_', '-') }.join('/')))
+        if @pg = (CmsPage.includes(:template).find_by_path(db_path.join('/')) || CmsPage.includes(:template).find_by_path(db_path.map { |segment| segment.gsub(/([A-Za-z\d])_/, '\1-') }.join('/')))
           if edit_mode
             redirect_to controller: '/management/cms', action: 'edit_page_content', id: @pg and return true
           else
