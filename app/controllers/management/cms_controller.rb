46c724825047a710b6c3ad21cc85fe3ce8fd3798
Aaron Namba <aaron@aaronnamba.com>
2017-08-10 01:46:07 +0000

    fix for previous force_encoding change

--- app/controllers/management/cms_controller.rb
+++ app/controllers/management/cms_controller.rb
@@ -481,7 +481,7 @@ class Management::CmsController < Management::ApplicationController # :nodoc:
             val.gsub!(/<br>(<\/h\d>|<\/p>)/, '\1')
           end
           
-          obj.content = val.force_encoding("UTF-8")
+          obj.content = val.to_s.force_encoding("UTF-8")
           obj.save
         end
         
